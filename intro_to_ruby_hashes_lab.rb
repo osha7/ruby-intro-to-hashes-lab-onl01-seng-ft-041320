ef9efe3dedda571c834b340cc2af381293f72df2
Osha G <ogroetz@gmail.com>
2020-04-18 19:58:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
 	     mortgage_value: "$100"
 	   },
 	   pennsylvania_railroad: {
-	     mortgage_value: "$200"
+	     "mortgage_value": "$200"
 	   },
 	   b_and_o_railroad: {
 	     mortgage_value: "4100"
